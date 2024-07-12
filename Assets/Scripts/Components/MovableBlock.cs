c47c831f755843c0fe5eefdac710bf5dca0dd976
William Skou Heidemann <Williamskou2227@gmail.com>
2024-07-12 09:24:42 +0200

    implement undo as press and hold

--- Assets/Scripts/Components/MovableBlock.cs
+++ Assets/Scripts/Components/MovableBlock.cs
@@ -2,15 +2,44 @@
 using Model;
 using Systems;
 using UnityEngine;
+using UtilityToolkit.Runtime;
 
 namespace Components
 {
     public class MovableBlock : MonoBehaviour, IInteractable
     {
         public Block model;
+        private Option<UndoTimer> undoTimer = Option<UndoTimer>.None;
         public void Interact()
         {
             Controller.Instance.Select(this);
+            undoTimer = Option<UndoTimer>.Some(new UndoTimer());
+        }
+
+        private void Update()
+        {
+            if (!undoTimer.IsSome(out var timer)) return;
+            if (!timer.isFinished) return;
+            Selector.Instance.Undo();
+            undoTimer = Option<UndoTimer>.None;
+        }
+
+        private void OnMouseUpAsButton()
+        {
+            if (!undoTimer.IsSome(out var timer)) return;
+            undoTimer = Option<UndoTimer>.None;
+        }
+
+        private class UndoTimer
+        {
+            public bool isFinished => timeOfCreation + TimeToFinish < Time.time;
+            public float percentDone => (Time.time - timeOfCreation) / TimeToFinish * 100;
+            private const float TimeToFinish = 0.5f;
+            private readonly float timeOfCreation;
+            public UndoTimer()
+            {
+                timeOfCreation = Time.time;
+            }
         }
     }
 }
